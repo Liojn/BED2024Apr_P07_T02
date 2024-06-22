fabc5097abdb4e82276e9d97126ade45d119e9ba
j0van-ong <s10258240@connect.np.edu.sg>
2024-06-22 19:51:46 +0800

    added seed.sql with table and data creation

--- seed.sql
+++ seed.sql
@@ -1,4 +1,4 @@
-/*User table created, for link to Event and others respectively*/
+
 CREATE TABLE Users (
   UserID INT PRIMARY KEY IDENTITY(1,1),
   Username NVARCHAR(255) NOT NULL UNIQUE,  
@@ -8,13 +8,26 @@ CREATE TABLE Users (
 
 
 CREATE TABLE Events (
-  EventID INT PRIMARY KEY IDENTITY(1,1),  -- Auto-incrementing integer as primary key
-  EventName VARCHAR(255) NOT NULL,       
-  EventDate DATE NOT NULL,                
-  StartTime DATETIME NOT NULL,             
-  EndTime DATETIME NOT NULL,               
-  Location VARCHAR(255),                   -- Location of the event
-  Description VARCHAR(500),                -- Detailed description of the event 
-  CreatedByUserID INT FOREIGN KEY REFERENCES Users(UserID),
-   CONSTRAINT chk_EndTimeAfterStartTime CHECK (StartTime <= EndTime)  -- Optional check constraint
+  eventId INT PRIMARY KEY IDENTITY(1,1),  -- auto-incrementing integer as primary key
+  title VARCHAR(255) NOT NULL,       
+  date DATE NOT NULL,                
+  startTime DATETIME NOT NULL,             
+  endTime DATETIME NOT NULL,               
+  location VARCHAR(255),                   -- Location of the event
+  description VARCHAR(500),                -- Detailed description of the event 
+  username NVARCHAR(255) FOREIGN KEY REFERENCES Users(Username),
+   CONSTRAINT chk_EndTimeAfterStartTime CHECK (StartTime <= EndTime)  -- check constraint
 );
+
+
+INSERT INTO Users (Username, Email, Password) 
+VALUES
+  ('user1', 'user1@example.com', 'password123'), 
+  ('user2', 'user2@example.com', 'password456'),  
+  ('user3', 'user3@example.com', 'password789');
+
+INSERT INTO Events (title, date, startTime, endTime, location, description, username)
+VALUES
+  ('Cleanshore Sembawang', '2024-08-23', '09:00:00', '12:00:00', 'Sembawang Beach', 'Join us for a day of cleaning up our beaches!', 'user1'),  
+  ('Eco Sprouts', '2024-09-14', '18:00:00', '21:00:00', 'Community Center Hall', 'Gear up and come planting with us!', 'user2'),  
+  ('Book Club Meeting', '2024-10-12', '10:00:00', '11:30:00', 'Library Meeting Room', 'Discuss the latest book selection!', 'user3'); 
