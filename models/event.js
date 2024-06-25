9ed00b3a17dd211f41f41ba31a25e245cf353b3b
j0van-ong <s10258240@connect.np.edu.sg>
2024-06-25 23:05:01 +0800

    added delete function

--- models/event.js
+++ models/event.js
@@ -23,13 +23,27 @@ class Event {
         const result = await request.query(sqlQuery);
 
         connection.close();
-        console.log(result);
+        //console.log(result);
 
         return result.recordset.map(
         (row) => 
             new Event(row.eventId, row.title, row.date, row.startTime, row.endTime, row.location, row.description, row.username)
         ); //convert row to event object
         
+    }
+
+    static async deleteEvent(id){
+        const connection = await sql.connect(dbConfig);
+    
+        const sqlQuery = `DELETE FROM Events WHERE eventId = @id`; // Parameterized query
+    
+        const request = connection.request();
+        request.input("id", id);
+        const result = await request.query(sqlQuery);
+    
+        connection.close();
+    
+        return result.rowsAffected > 0; // Indicate success based on affected rows
     }
 }
 
