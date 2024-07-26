9f04abb493058c15074cc47064a5c370cc54635b
Kiefer <S10259865@connect.np.edu.sg>
2024-07-26 15:05:26 +0800

    Chore: Fixed validation for feedback

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const feedbackController = require("./controllers/feedbackController");
 const eventController = require("./controllers/eventController");
 const userController = require("./controllers/userController");
 const donationController = require("./controllers/donationController");
+const validateEvent = require("./middleware/eventFormMiddleware");
 const notificationsController = require("./controllers/notificationsController");
 const dbConfig = require("./dbConfig");
 const bodyParser = require("body-parser");
@@ -14,13 +15,12 @@ const swaggerDocument = require("./swagger-output.json"); // Import generated sp
 
 const authMiddleware = require('./middleware/authMiddleware');
 const eventAuthorizeAction = require("./middleware/eventAuthorization");
-const validateEvent = require("./middleware/eventFormMiddleware");
 const { getDonationByUsername } = require("./models/donation");
-const { getDonationStatistics }= require("./models/donation");
 const app = express();
 const port = process.env.PORT || 3000;
 const staticMiddleware = express.static("public");
-
+const feedbackFormMiddleware = require("./middleware/feedbackFormMiddleware");
+const notificationFormMiddleware = require("./middleware/notificationFormMiddleware")
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(staticMiddleware);  
@@ -28,7 +28,7 @@ app.use(cors());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // Serve the Swagger UI at a specific route
 
 //Notifications Routes
-app.get("/notifications/userNotif/:Username",notificationsController.getNotificationsByUsername)
+app.get("/notifications/userNotif/:Username",authMiddleware,notificationsController.getNotificationsByUsername)
 app.get("/notifications/:id", authMiddleware,notificationsController.getNotificationById)
 app.post("/notifications",authMiddleware, notificationsController.createNotification)
 app.delete("/notifications/:id", authMiddleware,notificationsController.deleteNotification)
@@ -41,21 +41,20 @@ app.get("/notifications/seen/:seen/:username",authMiddleware,notificationsContro
 app.get("/feedbacks", authMiddleware,feedbackController.getAllFeedbacks);
 app.get("/feedbacks/:id", authMiddleware,feedbackController.getFeedbackById);
 app.delete("/feedbacks/:id", authMiddleware,feedbackController.deleteFeedback); 
-app.post("/feedbacks", authMiddleware,feedbackController.createFeedback);
+app.post("/feedbacks", feedbackFormMiddleware,authMiddleware,feedbackController.createFeedback);
 app.get("/feedbacks/verified/:verified", authMiddleware,feedbackController.getFeedbackByVerified);
 app.put("/feedbacks/:id", authMiddleware,feedbackController.updateFeedback)
 
 
 // Event Route
-app.get("/events/get-location", authMiddleware, eventController.getLocation);
-app.get("/events/search", authMiddleware, eventController.searchEvent);
+app.get("/events/get-location", eventController.getLocation);
+app.get("/events/search", eventController.searchEvent);
 
-app.get("/events/download/:id", authMiddleware, eventController.printPDFSummary);
 app.post("/events/register/:id", authMiddleware, eventController.registerEvent);
 app.get("/events", authMiddleware, eventController.getAllEvents);
 app.get("/events/:id", authMiddleware, eventController.getEventbyId);
-app.post("/events", authMiddleware, validateEvent, eventController.createEvent);
-app.put("/events/:id/update", authMiddleware, eventAuthorizeAction, validateEvent, eventController.updateEvent);
+app.post("/events", authMiddleware, eventController.createEvent);
+app.put("/events/:id/update", authMiddleware, eventAuthorizeAction, eventController.updateEvent);
 app.delete("/events/:id/deletion", authMiddleware, eventAuthorizeAction, eventController.deleteEvent);
 app.get("/events/find-participants/:id", authMiddleware, eventController.getUsersByEventId);
 
@@ -70,13 +69,10 @@ app.post('/users/login', userController.loginUser);
 
 
 // Donation routes
-app.get("/donations", authMiddleware, donationController.getAllDonations);
-app.get("/stats",authMiddleware,donationController.getAllStats)
-app.post("/donations",authMiddleware,donationController.createDonation);
+app.get("/donations", donationController.getAllDonations);
 app.get('/nonprofits', donationController.fetchNonProfitNames);
+app.post("/donations",donationController.createDonation);
 app.get("/donations/:username", donationController.getDonationByUsername);
-app.get("/stats",authMiddleware,donationController.getDonationStatistics)
-//donationController.getDonationStatistics()
 //app.get("/donations",donationController.getDonationCount)
 
 app.listen(port, async () => {
