8d7f06b0d20d5dcbbd15698195e39096722dec41
j0van-ong <s10258240@connect.np.edu.sg>
2024-07-26 17:47:00 +0800

    updated minor changes to swagger

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const staticMiddleware = express.static("public");
 const feedbackFormMiddleware = require("./middleware/feedbackFormMiddleware");
-const notificationFormMiddleware = require("./middleware/notificationFormMiddleware")
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(staticMiddleware);  
